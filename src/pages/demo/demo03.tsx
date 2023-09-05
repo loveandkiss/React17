83a44dd915c69de4a281c680b76fd1ab0c2bfccc
zhouyiping <zhouyiping_love@126.com>
2023-09-05 18:08:18 +0800

    跨组件通信，统一状态管理

--- src/pages/demo/demo03.tsx
+++ src/pages/demo/demo03.tsx
@@ -1,10 +1,20 @@
 // Demo03.tsx
-import React, { FC, useState, useCallback, useMemo } from 'react';
-import { Button } from 'antd'
-import MegaBoost from './components/MegaBoost';
+import React, { FC, useState, useCallback, useMemo } from "react";
+import { useHistory } from "react-router";
+import { Button } from "antd";
+import MegaBoost from "./components/MegaBoost";
+/**
+ * 在 React 组件中使用 Redux 状态和操作
+ */
+import useAppSelector from "../../hooks/useAppSelector";
+//  import useAppDispatch from "../../hooks/useAppDispatch";
 
 const Demo03: FC = () => {
   const [count, setCount] = useState(0);
+  // 获取history对象
+  const history = useHistory();
+  // 获取store数据
+  const storeCouter = useAppSelector((state) => state.counter.value);
 
   // function handleMegaBoost() {
   //   setCount((currentValue) => currentValue + 1234);
@@ -20,24 +30,28 @@ const Demo03: FC = () => {
   // 等价于(推荐使用)
   const handleMegaBoost = useCallback(() => {
     setCount((currentValue) => currentValue + 1234);
-  }, [])
+  }, []);
 
-  window.console.log('demo03')
+  window.console.log("demo03");
 
   return (
     <>
-      Count: {count}
+      <div>storeCouter: {storeCouter}</div>
+      <div>Count: {count}</div>
       <Button
         type="primary"
         danger
         onClick={() => {
-          setCount(count + 1)
+          setCount(count + 1);
         }}
       >
         Click me!
       </Button>
       <MegaBoost handleClick={handleMegaBoost} />
+      <div>
+        <Button onClick={() => history.push("/demo/04")}>Go Demo04 Page</Button>
+      </div>
     </>
   );
-}
+};
 export default Demo03;
--- src/pages/demo/demo04.tsx
+++ src/pages/demo/demo04.tsx
@@ -1,12 +1,16 @@
 // Demo04.tsx
 import React, { FC } from "react";
+import { useHistory } from "react-router";
+import { Button } from "antd";
 
 import Counter from "./components/Counter";
 
 const Demo04: FC = () => {
+  const history = useHistory();
   return (
     <>
       <Counter />
+      <Button onClick={() => history.push("/demo/03")}>Go Demo03 Page</Button>
     </>
   );
 };
